a728677e4f0e095a49d04b133215d46ed1c15ecd
Stacy-Sanders <stacy.sanders081@gmail.com>
2021-12-20 21:51:20 -0500

    inheritance

--- CSharpFundamentals/07_Inheritance/InheritanceTests.cs
+++ CSharpFundamentals/07_Inheritance/InheritanceTests.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace _07_Inheritance
 {
     [TestClass]
-    public class UnitTest1
+    public class InheritanceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PersonExamples()
         {
+            // var person = new Person();
+            // person.FirstName = "Stacy";
+            // person.ToString();
+
+            var customer = new Customer(true, "Stacy", "Sanders", "8125555555", "stacy.sanders@orbeez.com");
+            customer.ToString();
+            customer.FirstName = "Lawrence";
+            customer.IsPremium = true;
+
+            var employee = new Employee();
+            employee.FirstName = "Luke";
+            employee.HireDate = DateTime.Today;
+
+            var hourlyEmployee = new HourlyEmployee();
+
+
+            List<Person> people = new List<Person>();
+            // people.Add(person);
+            people.Add(customer);
+            people.Add(employee);
+            people.Add(hourlyEmployee);
+           
         }
     }
 }
